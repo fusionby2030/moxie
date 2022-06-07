a8535ac82fbff92c69013eae4556c6dd0a291571
fusionby2030 <adam.kit@helsinki.fi>
2022-06-07 17:57:42 +0300

    initalize experiment README

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "moxie", 
-    version="0.1.0",
-    packages = find_packages(), 
-    author="Adam Kit", 
-    author_email="adam.kit@helsinki.fi", 
-    description="Representation Learning of Pedestal profiles"
-
+    name = "moxie",
+    version="1.0.0",
+    packages = find_packages(),
+    author="Adam Kit",
+    author_email="adam.kit@helsinki.fi",
+    description="Representation Learning of Pedestal profiles",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     )
-
-
